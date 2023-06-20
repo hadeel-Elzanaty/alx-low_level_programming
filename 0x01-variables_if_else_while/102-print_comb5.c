8949651b06b4e4abf8f877c454241ce1e407215b
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-19 18:23:28 -0700

    combinatn

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,35 @@
-#
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description: combination of numbers
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 <= 99)
+	{
+		num2 = num1;
+		while (num2 <= 99)
+		{
+			if (num2 != num1)
+			{
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar(' ');
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
