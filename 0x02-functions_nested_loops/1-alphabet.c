87293ef9917c4a8700ba7378d4fb898b1e31e85c
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-20 15:42:16 -0700

    while

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet, in lowercase
+ * Return: Always 0(success)
+*/
+void print_alphabet(void)
+{
+	int ch = ('a');
+
+	while (ch <= 'z')
+	{
+		_putchar(ch)
+		ch++
+	}
+	_putchar('\n');
+	print_alphabet();
+	return (0);
+}
