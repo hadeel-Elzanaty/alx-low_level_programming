e40e19e2bc2dbf00925163466b83d65ff2f7a499
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-19 17:56:40 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description: combination of numbers
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0;
+	int num2;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				if (num1 < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description: combination of numbers
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0;
+	int num2;
+	int num3;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				if (num2 != num3 && num1 < num2 && num2 < num3 && num1 != num2)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
+					if (num1 < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					num3++;
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
