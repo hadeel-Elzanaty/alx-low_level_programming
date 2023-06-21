bc766aa014920220bf74b26fbb90d88cc87a1783
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-21 09:45:10 -0700

    for

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: adds two integers and returns the result
+ *
+ * Return: sum of a + b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
