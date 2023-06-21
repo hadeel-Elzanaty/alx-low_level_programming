b3821fe608ca7e4eafb2bd188565efb2f8f8e942
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-21 11:12:29 -0700

    for

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 /**
  * add - Entry point
  * Description: adds two integers and returns the result
- *
+ * @a: first input
+ * @b: second input
  * Return: sum of a + b
 */
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
