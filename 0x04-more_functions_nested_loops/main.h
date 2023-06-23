9497c8407052c883466a893fece81266b1823d9d
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-23 15:58:23 -0700

    prime

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,4 +14,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+void largest_prime_factor(long int num);
+double _sqrt(double num);
 #endif
