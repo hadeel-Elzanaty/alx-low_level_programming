f2dc35625705a7c75de2ff8d13a9998104885094
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-23 17:24:13 -0700

    prime

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,27 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 1; i <= n; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar(' ');
-			_putchar(92);
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+
+			}
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
+
+
