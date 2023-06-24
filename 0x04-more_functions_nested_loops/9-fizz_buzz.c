e6b4f408a08a266090c6cab54c499d0a440df46c
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-06-23 17:52:59 -0700

    prime

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point
  * Description: prints the numbers from 1 to 100
@@ -7,6 +7,22 @@
 */
 int main(void)
 {
-	int i;
+	int n;
 
+	for (n = 1; n >= 100; n++)
+	{
+		if ((n % 3  == 0) && (n % 5 != 0))
+			printf("Fizz");
+		else if ((n % 5  == 0) && (n % 3 != 0))
+			printf("Buzz");
+		else if ((n % 3  == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
