80a4da4f68500b3264e27ea51dceed6f184a797f
hadeel-Elzanaty <hadeelelzansty92@gmail.com>
2023-07-07 16:33:49 -0700

    tasks

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (success)
+**/
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcents = 0, m = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			leastcents += money / cents[i];
+			m = (m % cents[i] == 0)
+				break;
+		}
+		printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
